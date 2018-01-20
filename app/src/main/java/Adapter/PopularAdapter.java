7ba2f8902e8bb02856115baec9abe8ace27ceb3a
mastersunny <sunnycse248@gmail.com>
2018-01-20 13:15:42 +0600

    adding popular stories

---
+++ app/src/main/java/Adapter/PopularAdapter.java
@@ -0,0 +1,54 @@
+package Adapter;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import mastersunny.unitedclub.R;
+
+/**
+ * Created by sunnychowdhury on 1/19/18.
+ */
+
+public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.MainHolder> {
+
+    private ArrayList<String> list;
+    private Activity mActivity;
+
+    public PopularAdapter(Activity mActivity, ArrayList<String> list) {
+        this.mActivity = mActivity;
+        this.list = list;
+    }
+
+    @Override
+    public MainHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.most_used_item_layout, parent, false);
+        return new MainHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MainHolder holder, int position) {
+        if (list != null) {
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    public static class MainHolder extends RecyclerView.ViewHolder {
+
+
+        public MainHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/Fragments/MostUsedFragment.java
+++ app/src/main/java/Fragments/MostUsedFragment.java
@@ -63,8 +63,8 @@ public class MostUsedFragment extends Fragment implements View.OnClickListener {
         }
 
         most_used_rv = view.findViewById(R.id.most_used_rv);
-        most_used_rv.setLayoutManager(new LinearLayoutManager(mActivity, LinearLayoutManager.VERTICAL,false));
-        mostUsedAdapter = new MostUsedAdapter(mActivity,list);
+        most_used_rv.setLayoutManager(new LinearLayoutManager(mActivity, LinearLayoutManager.VERTICAL, false));
+        mostUsedAdapter = new MostUsedAdapter(mActivity, list);
         most_used_rv.setAdapter(mostUsedAdapter);
     }
 
