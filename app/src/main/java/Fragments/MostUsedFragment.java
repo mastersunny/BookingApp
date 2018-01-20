3021f73a3b5dbb48d3cce7e5e3d0ca1099a738c3
mastersunny <sunnycse248@gmail.com>
2018-01-20 16:21:08 +0600

    Retrofit using started

--- app/src/main/java/Fragments/MostUsedFragment.java
+++ app/src/main/java/Fragments/MostUsedFragment.java
@@ -8,30 +8,42 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import Adapter.MostUsedAdapter;
+import Adapter.MoviesAdapter;
+import mastersunny.unitedclub.Model.Movie;
+import mastersunny.unitedclub.Model.MoviesResponse;
 import mastersunny.unitedclub.R;
+import mastersunny.unitedclub.Rest.ApiClient;
+import mastersunny.unitedclub.Rest.ApiInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
  * Created by sunnychowdhury on 12/16/17.
  */
 
-public class MostUsedFragment extends Fragment implements View.OnClickListener {
+public class MostUsedFragment extends Fragment implements View.OnClickListener, Callback<MoviesResponse> {
 
+    public String TAG = "MostUsedFragment";
     private Activity mActivity;
     private View view;
     private RecyclerView most_used_rv;
     private SearchView searchView;
     private TextView no_client_message;
-    private ArrayList<String> list;
-    private MostUsedAdapter mostUsedAdapter;
+    private ArrayList<Movie> movies;
+    private MoviesAdapter moviesAdapter;
 
     @Override
     public void onAttach(Context context) {
@@ -44,10 +56,21 @@ public class MostUsedFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         if (view == null) {
             view = inflater.inflate(R.layout.fragment_layout, container, false);
+            movies = new ArrayList<>();
             initLayout();
+            loaData();
         }
 
         return view;
+    }
+
+    private void loaData() {
+        if (ApiClient.API_KEY.isEmpty()) {
+            Toast.makeText(mActivity, "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        apiService.getTopRatedMovies(ApiClient.API_KEY).enqueue(this);
     }
 
     @Override
@@ -57,19 +80,26 @@ public class MostUsedFragment extends Fragment implements View.OnClickListener {
     }
 
     private void initLayout() {
-        list = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            list.add("ddfhduifhuids " + i);
-        }
-
         most_used_rv = view.findViewById(R.id.most_used_rv);
         most_used_rv.setLayoutManager(new LinearLayoutManager(mActivity, LinearLayoutManager.VERTICAL, false));
-        mostUsedAdapter = new MostUsedAdapter(mActivity, list);
-        most_used_rv.setAdapter(mostUsedAdapter);
+        moviesAdapter = new MoviesAdapter(movies, R.layout.list_item_movie, mActivity);
+        most_used_rv.setAdapter(moviesAdapter);
     }
 
     @Override
     public void onClick(View v) {
+
+    }
+
+
+    @Override
+    public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+        movies.addAll(response.body().getResults());
+        moviesAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onFailure(Call<MoviesResponse> call, Throwable t) {
 
     }
 }
