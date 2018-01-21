7c64f2e2aaec175c01862f5cd0086beac5baf4f9
mastersunny <sunnycse248@gmail.com>
2018-01-21 16:02:25 +0600

    Push notification added

--- app/src/main/java/mastersunny/unitedclub/services/MyFirebaseMessagingService.java
+++ app/src/main/java/mastersunny/unitedclub/services/MyFirebaseMessagingService.java
@@ -1,10 +1,22 @@
 package mastersunny.unitedclub.services;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import mastersunny.unitedclub.Activity.MainActivity;
+import mastersunny.unitedclub.R;
 
 /**
  * Created by ASUS on 1/21/2018.
@@ -14,10 +26,33 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     public static final String TAG = "MyFirebaseMessagingService";
 
-    @SuppressLint("LongLogTag")
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        sendMyNotification(remoteMessage.getNotification().getBody());
+    }
+
+    @SuppressLint("LongLogTag")
+    private void sendMyNotification(String message) {
+        Log.d(TAG, "" + message);
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Notifications Example")
+                        .setContentText("This is a test notification");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("notify_001",
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+        manager.notify(0, builder.build());
     }
 }
