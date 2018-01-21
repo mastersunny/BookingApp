f8d6ceefc19523f67cf0cf45dc151ac7ca202a3b
mastersunny <sunnycse248@gmail.com>
2018-01-21 17:15:36 +0600

    Adding popular activity

---
+++ app/src/main/java/mastersunny/unitedclub/Adapter/PopularVerticalAdapter.java
@@ -0,0 +1,61 @@
+package mastersunny.unitedclub.Adapter;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import mastersunny.unitedclub.Model.PopularDTO;
+import mastersunny.unitedclub.R;
+
+/**
+ * Created by sunnychowdhury on 1/19/18.
+ */
+
+public class PopularVerticalAdapter extends RecyclerView.Adapter<PopularVerticalAdapter.MainHolder> {
+
+    private ArrayList<PopularDTO> popularDTOS;
+    private Activity mActivity;
+
+    public PopularVerticalAdapter(Activity mActivity, ArrayList<PopularDTO> popularDTOS) {
+        this.mActivity = mActivity;
+        this.popularDTOS = popularDTOS;
+    }
+
+    @Override
+    public MainHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.popular_item_layout, parent, false);
+        return new MainHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MainHolder holder, int position) {
+        if (popularDTOS != null) {
+            PopularDTO popularDTO = popularDTOS.get(position);
+            holder.company_name.setText(popularDTO.getCompanyName());
+            holder.total_offer.setText(popularDTO.getTotalOffer());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return popularDTOS == null ? 0 : popularDTOS.size();
+    }
+
+    public static class MainHolder extends RecyclerView.ViewHolder {
+
+
+        private TextView company_name;
+        private TextView total_offer;
+
+        public MainHolder(View itemView) {
+            super(itemView);
+            company_name = itemView.findViewById(R.id.company_name);
+            total_offer = itemView.findViewById(R.id.total_offer);
+        }
+    }
+}
---
+++ app/src/main/java/mastersunny/unitedclub/Model/PopularDTO.java
@@ -0,0 +1,44 @@
+package mastersunny.unitedclub.Model;
+
+/**
+ * Created by ASUS on 1/21/2018.
+ */
+
+public class PopularDTO {
+    private String companyName;
+    private int totalOffer;
+    private String imgUrl;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getTotalOffer() {
+        return totalOffer;
+    }
+
+    public void setTotalOffer(int totalOffer) {
+        this.totalOffer = totalOffer;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "PopularDTO{" +
+                "companyName='" + companyName + '\'' +
+                ", totalOffer=" + totalOffer +
+                ", imgUrl='" + imgUrl + '\'' +
+                '}';
+    }
+}
