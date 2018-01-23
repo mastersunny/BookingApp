f93d566f6975788aba5024a4b1ada20170b213be
mastersunny <sunnycse248@gmail.com>
2018-01-23 14:59:06 +0600

    Adding search activity

--- app/src/main/java/mastersunny/unitedclub/Activity/MainActivity.java
+++ app/src/main/java/mastersunny/unitedclub/Activity/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
