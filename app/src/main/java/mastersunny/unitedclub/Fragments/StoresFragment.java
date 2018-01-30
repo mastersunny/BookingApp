4d899818e128f213355978cae3d27f4b3f646990
mastersunny <sunnycse248@gmail.com>
2018-01-30 17:18:13 +0600

    Implemented Splash Screen

--- app/src/main/java/mastersunny/unitedclub/Fragments/StoresFragment.java
+++ app/src/main/java/mastersunny/unitedclub/Fragments/StoresFragment.java
@@ -2,6 +2,7 @@ package mastersunny.unitedclub.Fragments;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -19,6 +20,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import mastersunny.unitedclub.Activity.SearchActivity;
 import mastersunny.unitedclub.Adapter.PagerAdapter;
 import mastersunny.unitedclub.R;
 
@@ -154,10 +156,18 @@ public class StoresFragment extends Fragment implements View.OnClickListener {
         tabLayout = view.findViewById(R.id.tabLayout);
         viewPager = view.findViewById(R.id.viewPager);
 //        navigationView = view.findViewById(R.id.nav_view);
+
+        view.findViewById(R.id.search_button).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.search_button:
+                Intent intent = new Intent(v.getContext(), SearchActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                break;
+        }
     }
 }
