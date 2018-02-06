a5f85674f60c96a66c41e06c8411b419679e3a94
mastersunny <sunnycse248@gmail.com>
2018-02-06 17:05:38 +0600

    profile layout fixing

--- app/src/main/java/mastersunny/unitedclub/Activity/LoginActivity.java
+++ app/src/main/java/mastersunny/unitedclub/Activity/LoginActivity.java
@@ -2,23 +2,14 @@ package mastersunny.unitedclub.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-import mastersunny.unitedclub.Model.UserDTO;
 import mastersunny.unitedclub.R;
 
 /**
@@ -63,7 +54,8 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TransactionActivity.start(v.getContext(), new UserDTO());
+                Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(mainIntent);
                 finish();
             }
         });
