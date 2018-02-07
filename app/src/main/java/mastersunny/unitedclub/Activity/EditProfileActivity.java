af0d8a844f06c833e9510c5c21e1c923440d1a4f
mastersunny <sunnycse248@gmail.com>
2018-02-07 11:36:23 +0600

    adding merchant

--- app/src/main/java/mastersunny/unitedclub/Activity/EditProfileActivity.java
+++ app/src/main/java/mastersunny/unitedclub/Activity/EditProfileActivity.java
@@ -1,13 +1,18 @@
 package mastersunny.unitedclub.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.io.InputStream;
 
 import mastersunny.unitedclub.R;
 import mastersunny.unitedclub.utils.Constants;
@@ -17,6 +22,7 @@ public class EditProfileActivity extends AppCompatActivity implements View.OnCli
     private TextView first_name, last_name, phone_number, email, password;
     private ImageView change_cover_image;
     public static final int PICK_IMAGE = 1;
+    private String TAG = "EditProfileActivity";
 
 
     @Override
@@ -71,7 +77,13 @@ public class EditProfileActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PICK_IMAGE) {
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+
+            } catch (Exception e) {
+
+            }
             //TODO: action
         }
     }
