9c7adea9a4fb16ba256d0a4c1d2728c5b392cb4c
mastersunny <sunnycse248@gmail.com>
2018-02-18 12:37:50 +0600

    User registration initiating

--- app/src/main/java/mastersunny/unitedclub/Activity/MobileVerificationActivity.java
+++ app/src/main/java/mastersunny/unitedclub/Activity/MobileVerificationActivity.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import mastersunny.unitedclub.R;
 
 public class MobileVerificationActivity extends AppCompatActivity {
 
-    private Button btn_verify, btn_resend;
+    public String MobileVerificationActivity = "MobileVerificationActivity";
     private EditText one_time_password;
+    private TextView phone_number, timer_text;
+    private Button btn_next, btn_resend_code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +25,13 @@ public class MobileVerificationActivity extends AppCompatActivity {
     }
 
     private void initLayout() {
-        btn_verify = findViewById(R.id.btn_verify);
-        btn_resend = findViewById(R.id.btn_resend);
         one_time_password = findViewById(R.id.one_time_password);
+        phone_number = findViewById(R.id.phone_number);
+        timer_text = findViewById(R.id.timer_text);
+        btn_next = findViewById(R.id.btn_next);
+        btn_resend_code = findViewById(R.id.btn_resend_code);
 
-        btn_verify.setOnClickListener(new View.OnClickListener() {
+        btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
