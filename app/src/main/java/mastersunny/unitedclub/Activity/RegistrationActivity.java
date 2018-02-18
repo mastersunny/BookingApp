7dd70fee2b50c6de0bd92f15836994e49de4db73
mastersunny <sunnycse248@gmail.com>
2018-02-18 16:57:11 +0600

    User registration initiating

---
+++ app/src/main/java/mastersunny/unitedclub/Activity/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package mastersunny.unitedclub.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mastersunny.unitedclub.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
