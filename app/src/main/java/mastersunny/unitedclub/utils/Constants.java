8f99dba569589acfc0d49ef09e73f3112fa4fa85
mastersunny <sunnycse248@gmail.com>
2018-02-24 18:02:36 +0600

    swipe refresh added

--- app/src/main/java/mastersunny/unitedclub/utils/Constants.java
+++ app/src/main/java/mastersunny/unitedclub/utils/Constants.java
@@ -27,6 +27,14 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class Constants {
 
+    public static final String TOPIC_GLOBAL = "global";
+
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
     public static final int REQUEST_TIMEOUT = 5000;
     public static boolean debugOn = true;
     public static final int USER_TYPE_CLIENT = 1;
