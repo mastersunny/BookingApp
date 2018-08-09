50c2b0ba8735fa1f499d9d89b1ae615e34f7191c
mastersunny <sunny@inflack.com>
2018-08-09 17:53:47 +0600

    check in check out

--- app/src/main/java/mastersunny/unitedclub/rest/ApiClient.java
+++ app/src/main/java/mastersunny/unitedclub/rest/ApiClient.java
@@ -24,7 +24,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
     public static String TAG = "ApiClient";
 
-    public static String serverUrl = "http://9ec40bc0.ngrok.io/";
+    public static String serverUrl = "http://8763cfa3.ngrok.io/";
     public static final String BASE_URL = serverUrl;
     public static final String APP_NAME = "unitel/";
 
