c084c495d88a86799434a9bcd6713d992667f9f0
mastersunny <sunny@inflack.com>
2018-08-14 19:13:08 +0600

    exam list in home screen

--- app/src/main/java/mastersunny/unitedclub/activities/HomeActivity.java
+++ app/src/main/java/mastersunny/unitedclub/activities/HomeActivity.java
@@ -80,7 +80,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.home_activity);
         unbinder = ButterKnife.bind(this);
 
