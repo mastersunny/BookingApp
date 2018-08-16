1e6850eb31c70624463bcf4a735f8603b389d8ec
mastersunny <sunny@inflack.com>
2018-08-16 19:08:29 +0600

    room details

--- app/src/main/java/mastersunny/unitedclub/activities/SearchActivity.java
+++ app/src/main/java/mastersunny/unitedclub/activities/SearchActivity.java
@@ -103,7 +103,6 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
         apiInterface = ApiClient.createService(this, ApiInterface.class);
