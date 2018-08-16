91461aecbb749f86423a9d15b0671024e1f48a9b
mastersunny <sunny@inflack.com>
2018-08-16 15:32:43 +0600

    exam list in home screen

--- app/src/main/java/mastersunny/unitedclub/rest/ApiClient.java
+++ app/src/main/java/mastersunny/unitedclub/rest/ApiClient.java
@@ -24,7 +24,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
     public static String TAG = "ApiClient";
 
-    public static String serverUrl = "http://5ac19d59.ngrok.io/";
+    public static String serverUrl = "http://0d68f035.ngrok.io/";
     public static final String BASE_URL = serverUrl;
     public static final String APP_NAME = "unitel/";
 
