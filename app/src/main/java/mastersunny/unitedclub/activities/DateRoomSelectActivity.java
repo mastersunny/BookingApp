c4923af087dc0634518be85b034d78e2914d6591
mastersunny <sunny@inflack.com>
2018-08-27 15:16:30 +0600

    booking fragment change

--- app/src/main/java/mastersunny/unitedclub/activities/DateRoomSelectActivity.java
+++ app/src/main/java/mastersunny/unitedclub/activities/DateRoomSelectActivity.java
@@ -75,7 +75,7 @@ public class DateRoomSelectActivity extends AppCompatActivity implements DateSel
         if (savedInstanceState == null) {
             pagerAdapter.addFragment(new CheckinFragment(), "Start Date");
             pagerAdapter.addFragment(new CheckoutFragment(), "End Date");
-            pagerAdapter.addFragment(new RoomSelectionFragment(), "Room");
+//            pagerAdapter.addFragment(new RoomSelectionFragment(), "Room");
         } else {
             Integer count = savedInstanceState.getInt("tabsCount");
             String[] titles = savedInstanceState.getStringArray("titles");
@@ -103,7 +103,7 @@ public class DateRoomSelectActivity extends AppCompatActivity implements DateSel
 
             }
         });
-        viewPager.setOffscreenPageLimit(3);
+        viewPager.setOffscreenPageLimit(2);
         tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
         viewPager.setCurrentItem(selectedPosition);
