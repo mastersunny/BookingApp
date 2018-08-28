af0bc7b8fd9ce9e318d829447a6d0e8d7aa531f6
mastersunny <sunny@inflack.com>
2018-08-28 12:34:36 +0600

    room book

--- app/src/main/java/mastersunny/unitedclub/services/MyFirebaseMessagingService.java
+++ app/src/main/java/mastersunny/unitedclub/services/MyFirebaseMessagingService.java
@@ -1,40 +1,41 @@
-//package mastersunny.unitedclub.services;
-//
-//import android.app.PendingIntent;
-//import android.content.Context;
-//import android.content.Intent;
-//import android.support.v4.app.NotificationCompat;
-//import android.support.v4.app.NotificationManagerCompat;
-//import android.support.v4.content.LocalBroadcastManager;
-//
-//import com.google.firebase.messaging.FirebaseMessagingService;
-//import com.google.firebase.messaging.RemoteMessage;
-//
-//import org.json.JSONObject;
-//
-//import java.text.ParseException;
-//import java.text.SimpleDateFormat;
-//import java.util.Date;
-//
-//import mastersunny.unitedclub.activities.TransactionDetailsActivity;
-//import mastersunny.unitedclub.R;
-//import mastersunny.unitedclub.utils.Constants;
-//import mastersunny.unitedclub.utils.NotificationUtils;
-//
-///**
-// * Created by ASUS on 2/26/2018.
-// */
-//
-//public class MyFirebaseMessagingService extends FirebaseMessagingService {
-//
-//    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
-//    private NotificationUtils notificationUtils;
-//
-//    @Override
-//    public void onMessageReceived(RemoteMessage remoteMessage) {
-//
-//    }
-//
+package mastersunny.unitedclub.services;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import mastersunny.unitedclub.activities.TransactionDetailsActivity;
+import mastersunny.unitedclub.R;
+import mastersunny.unitedclub.utils.Constants;
+import mastersunny.unitedclub.utils.NotificationUtils;
+
+/**
+ * Created by ASUS on 2/26/2018.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
+    private NotificationUtils notificationUtils;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Constants.debugLog(TAG, remoteMessage.getNotification().getBody());
+
+    }
+
 //    private void handleDataMessage(JSONObject jsonObject) {
 //        try {
 //            int transactionId = jsonObject.getInt(Constants.TRANSACTION_ID);
@@ -94,33 +95,33 @@
 //
 //
 //    }
-//
-//    public static long getTimeMilliSec(String timeStamp) {
-//        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        try {
-//            Date date = format.parse(timeStamp);
-//            return date.getTime();
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//        return 0;
-//    }
-//
-//    /**
-//     * Showing notification with text only
-//     */
-//    private void showNotificationMessage(Context context, String title, String message, Intent intent) {
-//        notificationUtils = new NotificationUtils(context);
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-////        notificationUtils.showNotificationMessage(title, message, intent);
-//    }
-//
-//    /**
-//     * Showing notification with text and image
-//     */
-//    private void showNotificationMessageWithBigImage(Context context, String title, String message, String timeStamp, Intent intent, String imageUrl) {
-//        notificationUtils = new NotificationUtils(context);
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//        notificationUtils.showNotificationMessage(title, message, timeStamp, intent, imageUrl);
-//    }
-//}
+
+    public static long getTimeMilliSec(String timeStamp) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date date = format.parse(timeStamp);
+            return date.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /**
+     * Showing notification with text only
+     */
+    private void showNotificationMessage(Context context, String title, String message, Intent intent) {
+        notificationUtils = new NotificationUtils(context);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        notificationUtils.showNotificationMessage(title, message, intent);
+    }
+
+    /**
+     * Showing notification with text and image
+     */
+    private void showNotificationMessageWithBigImage(Context context, String title, String message, String timeStamp, Intent intent, String imageUrl) {
+        notificationUtils = new NotificationUtils(context);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        notificationUtils.showNotificationMessage(title, message, timeStamp, intent, imageUrl);
+    }
+}
