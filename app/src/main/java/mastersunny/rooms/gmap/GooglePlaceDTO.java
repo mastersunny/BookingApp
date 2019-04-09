c96ec00eb747f0ac44d8e3ba4327d796eb68fc37
sunnychowdhury <sunny@dginnovationlab.com>
2019-04-09 18:11:11 +0600

    place from google added

---
+++ app/src/main/java/mastersunny/rooms/gmap/GooglePlaceDTO.java
@@ -0,0 +1,41 @@
+
+package mastersunny.rooms.gmap;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class GooglePlaceDTO {
+
+    @SerializedName("predictions")
+    @Expose
+    private List<Prediction> predictions = null;
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    public List<Prediction> getPredictions() {
+        return predictions;
+    }
+
+    public void setPredictions(List<Prediction> predictions) {
+        this.predictions = predictions;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "GooglePlaceDTO{" +
+                "predictions=" + predictions +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
